905a2790d4f59ce194133aa389feed10df0ff1aa
Austin Copley <austinc5555@gmail.com>
2020-04-22 12:40:28 -0500

    defined load_file

--- 102/Week13-utility.py
+++ 102/Week13-utility.py
@@ -5,3 +5,12 @@
 
 def print_output(string):
     print("OUTPUT", string)
+
+def load_file(filename):
+    output_list = []
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            output_list = output_list.append(line)
+        return output_list
+
